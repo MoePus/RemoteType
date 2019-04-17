559ea82a1c777d6b9c76c55c2785d5f2fa9e1543
MoePus <547007249@qq.com>
2019-04-17 16:24:24 +0800

    Update RemoteType.h

--- RemoteType.h
+++ RemoteType.h
@@ -11,41 +11,12 @@ BOOL RPM(
 	size_t* lpNumberOfBytesRead
 );
 
-template<typename T>
-class ReflectPointer {
-public:
-	typedef T type;
-	T addr;
-	ReflectPointer(T _addr) :addr(_addr) {
-		static_assert(std::is_pointer<T>::value, "RemoteType size must be le than an registers.");
-	};
-	T address()
+struct _RemoteType_Deleter
+{
+	template <class T>
+	void operator() (T* p)
 	{
-		return addr;
-	}
-	operator const T() const
-	{
-		T result = (T)malloc(sizeof(std::remove_pointer<T>::type));
-		memset(result, 0, sizeof(std::remove_pointer<T>::type));
-		size_t rb;
-		RPM((void*)addr, result, sizeof(std::remove_pointer<T>::type), &rb);
-		return result;
-	};
-
-	template<typename U = std::remove_pointer<T>::type>
-	std::shared_ptr<U> share()
-	{
-		T result = (T)malloc(sizeof(std::remove_pointer<T>::type));
-		memset(result, 0, sizeof(std::remove_pointer<T>::type));
-		size_t rb;
-		RPM((void*)addr, result, sizeof(std::remove_pointer<T>::type), &rb);
-		return std::shared_ptr<U>(result, free);
-	};
-
-	template<typename W>
-	typename std::enable_if<std::is_pointer<W>::value, ReflectPointer<W>>::type cast()
-	{
-		return ReflectPointer<W>((W)addr);
+		free(p);
 	}
 };
 
@@ -53,20 +24,28 @@ template<typename T>
 class RemoteType {
 	size_t addr;
 public:
-	size_t address()
+	T* address()
 	{
-		return addr;
+		return (T*)addr;
 	}
 
-	RemoteType(size_t _addr) :addr(_addr) {
-		static_assert(sizeof(T) <= sizeof(size_t), "RemoteType size must be le than an registers.");
+	explicit RemoteType(size_t _addr) :addr(_addr) {
 	};
 
-	RemoteType(T _addr) :addr((size_t)_addr) {
+	explicit RemoteType(void* _addr) :addr((size_t)_addr) {
 	};
+
+	explicit RemoteType(T* _addr) :addr((size_t)_addr) {
+	};
+
+	explicit RemoteType() :addr(0) {
+	};
+
+	template<typename V>
+	RemoteType(V) = delete;
 
 	template<typename U = std::remove_pointer<T>::type>
-	typename std::enable_if<std::is_pointer<U>::value, RemoteType<U>>::type operator[] (const int index)
+	typename std::enable_if<std::is_pointer<T>::value, RemoteType<U>>::type operator[] (const int index)
 	{
 		T result;
 		size_t rb;
@@ -75,19 +54,66 @@ public:
 		return a;
 	};
 
-	template<typename U = std::remove_pointer<T>::type>
+	template<typename U = T>
 	typename std::enable_if<
-		!std::is_pointer<U>::value && (sizeof(U) <= sizeof(size_t)), U>::type
+		!std::is_pointer<U>::value, U>::type
 		operator[](const int index)
 	{
 		U result;
 		size_t rb;
-		RPM((void*)(addr + sizeof(U)*index), &result, sizeof(T), &rb);
+		RPM((void*)(addr + sizeof(U)*index), &result, sizeof(U), &rb);
 		return result;
 	};
 
+	template<typename U = T>
+	typename std::enable_if<!std::is_pointer<U>::value,
+		std::unique_ptr<U[], _RemoteType_Deleter> >::type
+		shareArray(int size) const
+	{
+		U* result = (U*)malloc(sizeof(T)*size);
+		size_t rb;
+		RPM((void*)addr,
+			result, sizeof(U)*size, &rb);
+		return std::unique_ptr<U[], _RemoteType_Deleter>(result);
+	}
+
 	template<typename U = std::remove_pointer<T>::type>
-	typename std::enable_if<std::is_pointer<U>::value, RemoteType<U>>::type operator*() const
+	typename std::enable_if<std::is_pointer<T>::value,
+		std::unique_ptr<RemoteType<U>[], _RemoteType_Deleter> >::type
+		shareArray(int size) const
+	{
+		RemoteType<U>* result = (RemoteType<U>*)malloc(sizeof(T)*size);
+		size_t rb;
+		RPM((void*)addr,
+			result, sizeof(T)*size, &rb);
+		return std::unique_ptr<RemoteType<U>[], _RemoteType_Deleter>(result);
+	}
+
+	T get() const
+	{
+		T result;
+		size_t rb;
+		RPM((void*)addr, &result, sizeof(T), &rb);
+		return result;
+	}
+
+	std::unique_ptr<T,_RemoteType_Deleter> share() const
+	{
+		T* result = (T*)malloc(sizeof(T));
+		size_t rb;
+		RPM((void*)addr,
+			result, sizeof(T), &rb);
+		return std::unique_ptr<T, _RemoteType_Deleter>(result);
+	}
+
+	operator const T() const
+	{
+		return this->get();
+	};
+
+	template<typename U = std::remove_pointer<T>::type>
+	typename std::enable_if<std::is_pointer<T>::value, RemoteType<U>>::type
+		operator*() const
 	{
 		T result;
 		size_t rb;
@@ -96,30 +122,87 @@ public:
 		return a;
 	}
 
-	template<typename U = std::remove_pointer<T>::type>
-	typename std::enable_if<!std::is_pointer<U>::value, U>::type operator*() const
+	template<typename U = T>
+	typename std::enable_if<!std::is_pointer<T>::value, U>::type
+		operator*() const
 	{
-		U result;
+		return this->get();
+	}
+
+	template<typename U = T>
+	typename std::enable_if<std::is_class<U>::value, std::unique_ptr<U, _RemoteType_Deleter>>::type
+	operator->() const noexcept
+	{	
+		U* result = (U*)malloc(sizeof(U));
 		size_t rb;
-		RPM((void*)(addr), &result, sizeof(U), &rb);
-		return result;
+		RPM((void*)(addr), result, sizeof(U), &rb);
+		return std::unique_ptr<U, _RemoteType_Deleter>(result);
 	}
 
 	template<typename U = std::remove_pointer<T>::type>
-	ReflectPointer<T> refl(int index = 0) const
+	typename std::enable_if<std::is_pointer<T>::value, RemoteType<U>>::type
+		operator->() const noexcept
 	{
-		return ReflectPointer<T>((T)((size_t)addr + sizeof(U)*index));
+		return this->operator*();
+	}
+
+	template<typename W>
+	RemoteType<W> cast()
+	{
+		return RemoteType<W>(addr);
+	}
+};
+
+template<typename T>
+class ReflectPointer {
+public:
+	size_t addr;
+	ReflectPointer(T _addr) :addr((size_t)_addr) {
+		static_assert(std::is_pointer<T>::value, "ReflectPointer must be a pointer.");
+	};
+	ReflectPointer() :addr(0) {
+		static_assert(std::is_pointer<T>::value, "ReflectPointer must be a pointer.");
+	};
+
+	T address()
+	{
+		return (T)addr;
 	}
 
 	template<typename U = std::remove_pointer<T>::type>
-	typename std::enable_if<std::is_pointer<U>::value,
-		std::shared_ptr<ReflectPointer<U>[]> >::type
-		reflArray(int size) const
+	RemoteType<U> toRemoteType() const
 	{
-		ReflectPointer<U>* result = (ReflectPointer<U>*)malloc(sizeof(U)*size);
-		size_t rb;
-		RPM((void*)addr,
-			result, sizeof(U)*size, &rb);
-		return std::shared_ptr<ReflectPointer<U>[]>(result, free);
+		return RemoteType<U>(addr);
+	}
+
+	template<typename W>
+	ReflectPointer<W> cast()
+	{
+		return ReflectPointer<W>((W)addr);
+	}
+
+	template<typename U = std::remove_pointer<T>::type>
+	RemoteType<U> operator->() const
+	{
+		return toRemoteType();
+	}
+
+	template<typename U = std::remove_pointer<T>::type>
+	U get() const
+	{
+		return toRemoteType().get();
+	}
+
+	template<typename U = std::remove_pointer<T>::type>
+	std::unique_ptr<U, _RemoteType_Deleter> share() const
+	{
+		return toRemoteType().share();
+	}
+
+	template<typename U = std::remove_pointer<T>::type>
+	typename std::enable_if<std::is_pointer<U>::value, ReflectPointer<U>>::type
+		operator*() const
+	{
+		return ReflectPointer<U>(this->get());
 	}
 };
